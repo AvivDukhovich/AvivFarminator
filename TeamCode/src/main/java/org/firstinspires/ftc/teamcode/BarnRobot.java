cfa763d03cd52a01a1c200e1f13a660fe2970189
AvivDukhovich <avivaviv11223344@gmail.com>
2025-10-07 07:22:26 +0300

    cleaned up structure a bit more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarnRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarnRobot.java
@@ -1,35 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+public class BarnRobot {
 
-import com.seattlesolvers.solverslib.command.Robot;
-import com.seattlesolvers.solverslib.gamepad.GamepadEx;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.subsystems.ArmSubSystem;
-import org.firstinspires.ftc.teamcode.subsystems.ClawSubSystem;
-
-public class BarnRobot extends Robot {
-    public static BarnRobot instance;
-    public ClawSubSystem claw;
-    public ArmSubSystem arm;
-    public GamepadEx gamepadEx1;
-    public GamepadEx gamepadEx2;
-    public Telemetry telemetry;
-
-    public static synchronized BarnRobot getInstance() {
-        if (instance == null) {
-            instance = new BarnRobot();
-        }
-        return instance;
-    }
-
-    public void initBarnRobotSystems() {
-        gamepadEx1 = new GamepadEx(gamepad1);
-        gamepadEx2 = new GamepadEx(gamepad2);
-
-        claw = new ClawSubSystem();
-        arm = new ArmSubSystem();
-    }
 }
