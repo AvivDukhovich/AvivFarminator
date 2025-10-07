ca5c1b01d0571088c5fc7019811a34f645fad212
AvivDukhovich <avivaviv11223344@gmail.com>
2025-10-07 06:57:44 +0300

    Copied the limelight-auto-alignment branch from Main Project

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarnRobot.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.Robot;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.SubSystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.SubSystems.LimeLight;
+import org.firstinspires.ftc.teamcode.SubSystems.Transfer;
+import org.firstinspires.ftc.teamcode.Utils.GlobalData;
+
+public class BarnRobot extends Robot {
+
+    // ------------------------------------------------------------
+    // Singleton Instance
+    // ------------------------------------------------------------
+    private static BarnRobot instance;
+
+    // ------------------------------------------------------------
+    // Subsystems
+    // ------------------------------------------------------------
+    public Transfer transfer;
+    public DriveTrain drive;
+    public LimeLight limelight;
+
+    // ------------------------------------------------------------
+    // Gamepads
+    // ------------------------------------------------------------
+    public GamepadEx gamepadEx1;
+    public GamepadEx gamepadEx2;
+
+    // ------------------------------------------------------------
+    // FTC SDK References
+    // ------------------------------------------------------------
+    public Telemetry telemetry;
+    public HardwareMap hardwareMap;
+
+    public GlobalData.Alliance teamColor;
+    // ------------------------------------------------------------
+    // Singleton Accessor
+    // ------------------------------------------------------------
+    public static synchronized BarnRobot getInstance() {
+        if (instance == null) {
+            instance = new BarnRobot();
+        }
+        return instance;
+    }
+
+    // ------------------------------------------------------------
+    // Initialization
+    // ------------------------------------------------------------
+    public void initBarnRobotSystems(HardwareMap hw, Gamepad gamepad1, Gamepad gamepad2, GlobalData.Alliance teamColor) {
+        this.hardwareMap = hw;
+        this.teamColor = teamColor;
+
+        // Initialize Gamepads
+        gamepadEx1 = new GamepadEx(gamepad1);
+        gamepadEx2 = new GamepadEx(gamepad2);
+
+        // Initialize Subsystems
+        initTransfer();
+        initDrivetrain();
+        initLimeLight();
+    }
+
+    public void initDrivetrain() {
+        drive = new DriveTrain();
+        drive.setDefaultCommand(drive.driveCommand());
+    }
+
+    public void initTransfer() {
+        transfer = new Transfer();
+    }
+
+    public void initLimeLight() {
+        limelight = new LimeLight();
+        limelight.start();
+    }
+}
